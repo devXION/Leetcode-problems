3e841d8188d2dc6945a514133d6910d4ae06d18d
XION <83879915+devXION@users.noreply.github.com>
2022-06-25 20:23:45 +0200

    Add files via upload
    simple code to find the longest common prefix in multiple words given

---
+++ common prefix.py
@@ -0,0 +1,10 @@
+def longestCommonPrefix(self, strs: List[str]) -> str:
+    if len(strs) == 0:
+        return ""
+    s = strs[0]
+    for i in range(1, len(strs)):
+        while strs[i][0:len(s)] != s:
+            s = s[0:len(s) - 1]
+            if len(s) == 0:
+                return ""
+    return s
