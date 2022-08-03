33465fd599b05284328cfe94d2437311e50e5f4d
XION <83879915+devXION@users.noreply.github.com>
2022-08-03 11:45:44 +0200

    Update 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.py

--- 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.py
+++ 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.py
@@ -1,3 +1,8 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
         return max(n)
+
+assert max('32') == '3'
+assert max('82734') == '8'
+assert max('27346209830709182346') == '9'
+assert max('40') == '4'
