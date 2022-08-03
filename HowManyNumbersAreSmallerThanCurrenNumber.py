b98cbf8202ca8db7dd4d82834c4241a5eee627fb
XION <83879915+devXION@users.noreply.github.com>
2022-08-03 11:52:42 +0200

    Update HowManyNumbersAreSmallerThanCurrenNumber.py

--- HowManyNumbersAreSmallerThanCurrenNumber.py
+++ HowManyNumbersAreSmallerThanCurrenNumber.py
@@ -4,3 +4,6 @@ class Solution:
         for i,n in enumerate(sorted(nums)):
             if n not in d: d[n]=i
         return [d[i] for i in nums]
+assert  nums == [8,1,2,2,3]
+assert  nums == [6,5,4,8]
+assert  nums == [7,7,7,7]
