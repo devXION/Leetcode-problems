34ff91d9b5b4e374649cd0306fabad9ee68ac017
XION <83879915+devXION@users.noreply.github.com>
2022-08-03 16:41:20 +0200

    Update HowManyNumbersAreSmallerThanCurrenNumber.py

--- HowManyNumbersAreSmallerThanCurrenNumber.py
+++ HowManyNumbersAreSmallerThanCurrenNumber.py
@@ -1,9 +1,9 @@
-class Solution:
-    def smallerNumbersThanCurrent(self, nums):
-        d = {}
-        for i,n in enumerate(sorted(nums)):
-            if n not in d: d[n]=i
-        return [d[i] for i in nums]
-assert  nums == [8,1,2,2,3]
-assert  nums == [6,5,4,8]
-assert  nums == [7,7,7,7]
+def smallerNumbersThanCurrent(nums):
+    d = {}
+    for i, n in enumerate(sorted(nums)):
+        if n not in d: d[n] = i
+    return [d[i] for i in nums]
+
+
+def tester():
+    smallerNumbersThanCurrent([6,5,4,8]) == [2,1,0,3]
