b383260dbb66dc02888f81353ef4edeabcac18a1
XION <83879915+devXION@users.noreply.github.com>
2022-08-03 16:40:37 +0200

    Update PalindromeNumber.py

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -1,6 +1,8 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        if(str(x) == str(x)[::-1]):
-            return True
-        else:
-            return False
+def isPalindrome(
+        x: int) -> bool:
+    if (str(x) == str(x)[::-1]):
+        return True
+    else:
+        return False
+def tester():
+    isPalindrome(121) == True
