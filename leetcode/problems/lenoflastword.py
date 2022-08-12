fe10a83916605b7490bcf9b1ed9651d5a07fb8d5
devXION <xionroxard@gmail.com>
2022-08-12 11:05:05 +0200

    Updated version of commit

--- leetcode/problems/lenoflastword.py
+++
@@ -1,5 +0,0 @@
-import pytest
-def lengthOfLastWord(s: str) -> int:
-    lastword = s.split()
-    return len(lastword[-1])
-
--- leetcode/tests/lenoflastword_test.py
+++
@@ -1,3 +0,0 @@
-@pytest.mark.parametrize("word, number", [("joyboy", 6)])
-def tester(word,number):
-    assert lengthOfLastWord(word) == number
